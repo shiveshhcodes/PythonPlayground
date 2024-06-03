82012443e234259eed310e7c4e00a7a56777ab88
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-04 01:03:35 +0530

    Create speedtest1.py

---
+++ speedtest_tkinter/speedtest1.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import speedtest
+
+sp = Tk()
+sp.title("Internet Speed")
+sp.geometry("560x580")
+sp.config(bg="green")
+
+lab = Label(sp,text="Internet Speed Test" , font=("Time New Roman" , 30 , "bold"))
+lab.place(x=40,y=40)
+
+sp.mainloop()
