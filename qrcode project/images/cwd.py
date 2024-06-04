138b775e6a385e42727852b2104b784887ea8361
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-05 00:40:53 +0530

    Create cwd.py

---
+++ qrcode project/images/cwd.py
@@ -0,0 +1,17 @@
+import os
+
+# Define the directory path
+directory = "qrcode project/images"
+
+# Get the directory name
+directory_name = os.path.abspath(directory)
+
+print("Directory name:", directory_name)
+
+# import os
+
+# # Get the current working directory
+# cwd = os.getcwd()
+
+# print("Current Working Directory:", cwd)
+
