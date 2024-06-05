38b1173679f986428fcaee13d8b959605d2b11e1
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-06 04:34:57 +0530

    Update code.py

--- password manager/code/code.py
+++ password manager/code/code.py
@@ -1,8 +1,6 @@
 import os
 from cryptography.fernet import Fernet
 
-master_pwd = input("What is the master password? ")
-
 '''
 def write_key():
     keyfolder_path = '/Users/shiveshrichhariya/Desktop/GITHUB/Python Projects/PythonPlayground/password manager/encrypted key'
@@ -12,12 +10,16 @@ def write_key():
     key = Fernet.generate_key()
     with open(keyfile_path , "wb") as key_file:
         key_file.write(key) '''
-    
+        
 def load_key():
     file = open("key.key" , "rb")
     key = file.read()
     file.close()
     return key
+
+master_pwd = input("What is the master password? ")
+key = load_key() + master_pwd.encode() 
+fer = Fernet
 def add():
     folder_path = '/Users/shiveshrichhariya/Desktop/GITHUB/Python Projects/PythonPlayground/password manager/.txt files'
     file_path = os.path.join(folder_path , 'password.txt')
@@ -26,7 +28,7 @@ def add():
     name = input("Enter the name: ")
     pwd = input("Enter the password: ")
     with open(file_path, 'a') as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode().decode() + "\n"))
         
 
 def view():
@@ -34,7 +36,7 @@ def view():
         for line in f.readlines():
             data = (line.rstrip())
             user , passw = data.split("|")
-            print("User:" , user , "| Password:" , passw)
+            print("User:" , user , "| Password:" , (fer.decrypt(passw.encode())).decode())
 
 while True:
     mode = input("What do you want to do with the password? view/add and q to quit - ").lower()
