400d85d99146366a3d96c0dbf505354b44b91876
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-05 21:16:39 +0530

    Create code.py

---
+++ games fun project/quiz game/code.py
@@ -0,0 +1,32 @@
+print("welcome to my computer quiz!!")
+
+playing = input("do you want to play?? ")
+
+if playing.lowerll != "yes":
+    quit()
+    
+print("let's start playing")
+score = 0
+
+answer = input("who's gonna be prime minister of india?? ")
+if answer == "Narendra Modi":
+    print('correct!!')
+    score +=1
+else:
+    (print('incorrect'))
+    
+answer = input("who's gonna be prime minister of india?? ")
+if answer == "Narendra Modi":
+    print('correct!!')
+    score +=1
+else:
+    (print('incorrect'))
+    
+answer = input("who's gonna be prime minister of india?? ")
+if answer == "Narendra Modi":
+    print('correct!!')
+    score +=1
+else:
+    (print('incorrect'))
+
+print("you have got" + str(score) + "questions correct!!")
