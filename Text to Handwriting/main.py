72523d460ce171ad3ba6bf470a30cac306e3c523
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-07 23:25:13 +0530

    Generate Handwriting of Text

---
+++ Text to Handwriting/main.py
@@ -0,0 +1,8 @@
+import pywhatkit as pw
+txt = '''
+hi my name is shivesh , i am a python developer ,i am 4th year UG student ,
+and currently i am doing 100 days of code in twitter with python.
+
+'''
+pw.text_to_handwriting(txt,[0,0,138])
+print(" END ")
