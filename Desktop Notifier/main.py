0bbaf7ecbb86d91a10e30883a15a573471e15038
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-07 23:50:37 +0530

    improved code

--- Desktop Notifier/main.py
+++ Desktop Notifier/main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
         notification.notify (
             title = " REST NOW " ,
             message = "rest karle bhai" ,
-            app_icon="/Users/shiveshrichhariya/Downloads/search.png",
+            # app_icon="/Users/shiveshrichhariya/Downloads/search.png",
             timeout=4,
         )
         time.sleep(10)
