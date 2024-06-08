17a87352816fe6d0ec11a827039dbb5e5c87d67d
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-08 19:43:26 +0530

    Phone Numbers Details

---
+++ Phone No. Info/main.py
@@ -0,0 +1,20 @@
+import phonenumbers
+from phonenumbers import timezone,geocoder,carrier
+def ensure_with(no):
+    if not no.startswith('+91'):
+        no = '+91' + no
+    return no
+
+number = input("Enter your number with +91: ")
+number = ensure_with(number)
+try:
+    phone = phonenumbers.parse(number)
+    time=timezone.time_zones_for_number(phone)
+    car=carrier.name_for_number(phone,"en")
+    reg=geocoder.description_for_number(phone,"en")
+    print(phone)
+    print(time)
+    print(car)
+    print(reg)
+except phonenumbers.phonenumberutil.NumberParseException as e:
+    print(f"Error parsing number: {e}")
