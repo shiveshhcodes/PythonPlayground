1743dd03fdee1687e3f214adf86aa66c1922a8a9
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-08 23:49:04 +0530

    code upgrade.

--- Youtube Video Downloader/main.py
+++ Youtube Video Downloader/main.py
@@ -9,6 +9,6 @@ from pytube import YouTube
 
 YouTube_1 = YouTube("https://www.youtube.com/watch?v=giYejigUM9A")
 print("The Title of Video is " , YouTube_1.title)
-# print(YouTube_1.thumbnail_url)
+print(YouTube_1.thumbnail_url)
 # print(YouTube_1.views)
 # print(YouTube_1.author)
