05b26c9585bab5ea00969db82754b7aab95b720a
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-08 20:33:36 +0530

    Block Website for Specific Duration

---
+++ Website Blocker/main.py
@@ -0,0 +1,7 @@
+# little confusion is there , will complete it later.
+
+# import datetime 
+
+# end_time = datetime.datetime(2024,6,8)
+# site_block = ["https://www.ndtv.com" , "https://www.hotstar.com"]
+# host_path = "/private/etc/hosts directory/hosts"
