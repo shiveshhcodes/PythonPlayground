4a70a579f54fd568c03b5a952d21430105758b7d
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-08 19:47:04 +0530

    added send message button

--- Instagram Automate/main.py
+++ Instagram Automate/main.py
@@ -4,7 +4,7 @@ bot.login(username="shiveshhhhhhh" , password='zakmef-Jizjo7-topmyv')
 bot.follow('https://www.instagram.com/rajat_9629/#')
 bot.upload_photo("")
 bot.unfollow("")
-# bot.send_message("HI" , ['warpaintjournal'])
+bot.send_message("HI" , ['warpaintjournal'])
 
 # followers =bot.get_user_followers("shiveshhhhhhh")
 # for follower in followers:
