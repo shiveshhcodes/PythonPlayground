ba7d65c655661c7c5e1a2a35cc20faf119b13a77
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-08 19:49:18 +0530

    name updated

---
+++ Instagram Bot/main.py
@@ -0,0 +1,12 @@
+from instabot import Bot
+bot=Bot()
+bot.login(username="shiveshhhhhhh" , password='zakmef-Jizjo7-topmyv')
+# bot.follow('https://www.instagram.com/rajat_9629/#')
+# bot.upload_photo("")
+# bot.unfollow("")
+# bot.send_message("HI" , ['warpaintjournal'])
+
+followers =bot.get_user_followers("shiveshhhhhhh")
+for follower in followers:
+    print(bot.get_user_info(follower))
+
