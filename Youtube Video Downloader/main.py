c9bdbdb69191320a473b2de90f8a8b087cc2c4ad
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-09 23:46:40 +0530

    added some comments

--- Youtube Video Downloader/main.py
+++ Youtube Video Downloader/main.py
@@ -4,9 +4,10 @@ import certifi
 ssl._create_default_https_context = ssl._create_unverified_context
 ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=certifi.where())
 
-# Your existing code
+
 from pytube import YouTube
 
+# video link here
 YouTube_1 = YouTube("https://www.youtube.com/watch?v=giYejigUM9A")
 print("The Title of Video is " , YouTube_1.title)
 print(YouTube_1.thumbnail_url)
