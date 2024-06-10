771cfa9410c977edf47e73fc69b9f6091f5970e3
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-10 23:30:59 +0530

    Update main.py

--- Desktop Notifier/main.py
+++ Desktop Notifier/main.py
@@ -4,12 +4,12 @@
 from plyer import notification
 import time
 
-if __name__ == '__main__':
-    while True:
-        notification.notify (
-            title = " REST NOW " ,
-            message = "Rest Time Dudeeeeee!!" ,
-            # app_icon="/Users/shiveshrichhariya/Downloads/search.png",
-            timeout=5,
-        )
-        time.sleep(10)
+# if __name__ == '__main__':
+while True:
+    notification.notify (
+        title = " REST NOW " ,
+        message = "Rest Time Dudeeeeee!!" ,
+        # app_icon="/Users/shiveshrichhariya/Downloads/search.png",
+        timeout=5,
+    )
+    time.sleep(13)
