2ef2aa11e1621ef673754cad7bd705543d431d5c
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-13 00:51:46 +0530

    added features

--- Phone No. Info/practice1.py
+++ Phone No. Info/practice1.py
@@ -1,8 +1,21 @@
 import phonenumbers
+from phonenumbers import format_number , PhoneNumberFormat , is_valid_number ,  region_code_for_number , geocoder
 
-number = phonenumbers.parse("9425813786" , "IN")
-# print(number)
+print("entered number is - 9425813786\nand details of the numbers are ⬇️\n")
 
-from phonenumbers import format_number , PhoneNumberFormat
+number = phonenumbers.parse("9425813786" , "IN" ,)
+print(number)
+
+is_valid = is_valid_number(number)
+print("This number valids according to region" , is_valid)
+
 formatted_number = format_number(number , PhoneNumberFormat.INTERNATIONAL)
-print(formatted_number)
+print("The International format of this number is",formatted_number)
+
+region_code = region_code_for_number(number)
+print("The region code for the entered numeber is" , region_code)
+
+description = geocoder.description_for_number(number , "en")
+print("This number belongs to Country", description)
+
+
