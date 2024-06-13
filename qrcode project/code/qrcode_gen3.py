55bc28ea8c03e387a2222bbeb879e493d3e38b4e
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-13 19:38:01 +0530

    Create qrcode_gen3.py

---
+++ qrcode project/code/qrcode_gen3.py
@@ -0,0 +1,16 @@
+import os
+import qrcode
+
+# qr = qrcode.QRCode()
+# qr.add_data('https://chatgpt.com/c/8e0c7016-79fd-43e6-af2c-e574b7c726fa')
+# qr.make(fit=True)
+
+# img = qr.make_image(fill='black' , back_color='white')
+# img.show()
+
+
+# directory = "/Users/shiveshrichhariya/Desktop/GITHUB/Python Projects/images"
+# file_name = "googlee.png"
+
+# file_path = os.path.join(directory , file_name)
+# img.save(file_path)
