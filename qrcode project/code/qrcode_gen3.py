18eae8e58d729beff3023bc139bbcdec4eebcd5a
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-13 19:55:01 +0530

    Update qrcode_gen3.py

--- qrcode project/code/qrcode_gen3.py
+++ qrcode project/code/qrcode_gen3.py
@@ -1,5 +1,7 @@
 import os
 import qrcode
+from PIL import Image
+import qrcode.constants
 
 # basic code
 
@@ -10,7 +12,11 @@ import qrcode
 # img = qr.make_image(fill='black' , back_color='white')
 # img.show()
 
+
 # Creating a QR code with custom size and border
+
+
+
 # qr = qrcode.QRCode(
 #     version=1,
 #     error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -23,14 +29,38 @@ import qrcode
 # img = qr.make_image(fill='black', back_color='white')
 # img.show()
 
-# text image qr code
-qr = qrcode.QRCode()
-qr.add_data('Hello , World!')
-qr.make(fit = True)
-img = qr.make_image(fill='black' , back_color='white')
-# img.show()
-directory = "/Users/shiveshrichhariya/Desktop/GITHUB/Python Projects/PythonPlayground/qrcode project/images"
-file_name = "helloworld.png"
 
-file_path = os.path.join(directory , file_name)
-img.save(file_path)
+
+# text image qr code
+
+
+# qr = qrcode.QRCode()
+# qr.add_data('Hello , World!')
+# qr.make(fit = True)
+# img = qr.make_image(fill='black' , back_color='white')
+# # img.show()
+# directory = "/Users/shiveshrichhariya/Desktop/GITHUB/Python Projects/PythonPlayground/qrcode project/images"
+# file_name = "helloworld.png"
+
+# file_path = os.path.join(directory , file_name)
+# img.save(file_path)
+
+
+qr = qrcode.QRCode(
+    version = 1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size = 10,
+    border = 4,
+)
+
+qr.add_data("Sam Altman")
+qr.make(fit=True)
+
+img = qr.make_image(fill = 'black' , back_color = 'white')
+logo = Image.open('/Users/shiveshrichhariya/Desktop/GITHUB/Python Projects/PythonPlayground/qrcode project/images/google.png')
+logo = logo.resize((90, 90))
+pos = ((img.size[0] - logo.size[0]) // 2, (img.size[1] - logo.size[1]) // 2)
+img.paste(logo, pos)
+
+img.show()
+# img.show()
