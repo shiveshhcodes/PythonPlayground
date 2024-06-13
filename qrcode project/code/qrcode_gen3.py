53072552ea0652fcae421f2902dbb4906e968257
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-13 19:38:54 +0530

    Update qrcode_gen3.py

--- qrcode project/code/qrcode_gen3.py
+++ qrcode project/code/qrcode_gen3.py
@@ -1,12 +1,27 @@
 import os
 import qrcode
 
+# basic code
+
 # qr = qrcode.QRCode()
 # qr.add_data('https://chatgpt.com/c/8e0c7016-79fd-43e6-af2c-e574b7c726fa')
 # qr.make(fit=True)
 
 # img = qr.make_image(fill='black' , back_color='white')
 # img.show()
+
+# Creating a QR code with custom size and border
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('https://www.example.com')
+qr.make(fit=True)
+
+img = qr.make_image(fill='black', back_color='white')
+img.show()
 
 
 # directory = "/Users/shiveshrichhariya/Desktop/GITHUB/Python Projects/images"
