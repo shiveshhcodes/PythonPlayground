bd67ec211677a296d5da1df2c3f3538dd807e546
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-13 19:45:40 +0530

    Update qrcode_gen3.py

--- qrcode project/code/qrcode_gen3.py
+++ qrcode project/code/qrcode_gen3.py
@@ -11,21 +11,26 @@ import qrcode
 # img.show()
 
 # Creating a QR code with custom size and border
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data('https://www.example.com')
-qr.make(fit=True)
+# qr = qrcode.QRCode(
+#     version=1,
+#     error_correction=qrcode.constants.ERROR_CORRECT_L,
+#     box_size=10,
+#     border=4,
+# )
+# qr.add_data('https://www.example.com')
+# qr.make(fit=True)
 
-img = qr.make_image(fill='black', back_color='white')
-img.show()
+# img = qr.make_image(fill='black', back_color='white')
+# img.show()
 
+# text image qr code
+qr = qrcode.QRCode()
+qr.add_data('Hello , World!')
+qr.make(fit = True)
+img = qr.make_image(fill='black' , back_color='white')
+# img.show()
+directory = "/Users/shiveshrichhariya/Desktop/GITHUB/Python Projects/PythonPlayground/qrcode project/images"
+file_name = "helloworld.png"
 
-# directory = "/Users/shiveshrichhariya/Desktop/GITHUB/Python Projects/images"
-# file_name = "googlee.png"
-
-# file_path = os.path.join(directory , file_name)
-# img.save(file_path)
+file_path = os.path.join(directory , file_name)
+img.save(file_path)
